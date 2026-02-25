070f3456f499a044787be43ddf921736afbb92e2
Aditya Rawat <adityarawatnew2487@gmail.com>
2026-02-25 13:41:35 +0530

    written zod validations

---
+++ models/User.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface UserDocumentInterface extends Document{
+    fullName:string,
+    phoneNumber:number,
+    email:string,
+    merchantId:string,
+    password:string
+}
+
+const UserSchema:Schema<UserDocumentInterface > = new Schema({
+    fullName:{
+        type:String,
+        required:true,
+    },
+    phoneNumber:{
+        type:Number,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    merchantId:{
+        type:String
+    }
+},{
+    timestamps:true
+})
+
+const User = mongoose.models.User || mongoose.model<UserDocumentInterface>("User", UserSchema)
+export default User
